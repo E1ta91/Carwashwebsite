8525f7a738acb6ac72235be7b1938914bc068c3a
Elsie <elsietani117@gmail.com>
2024-07-31 11:28:45 +0000

    not done with home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
+    <div>
+      
+      <h1 className='text-center text-3xl flex justify-center items-center'>
+              Click here to <a href="#" onClick={() => navigate('/signup')} className='text-blue-600 font-semibold'>Sign up</a>
+            </h1>
+
+
+    </div>
   )
 }
 
